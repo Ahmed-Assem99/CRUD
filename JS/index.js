0ba47ab64cbdcbbf9f4fd9cac58e9005841da7a4
Ahmed El Fakharany <ahmedassem13@gmail.com>
2026-07-16 12:53:04 +0300

    added search feature

--- JS/index.js
+++ JS/index.js
@@ -2,11 +2,12 @@ var productNameInput = document.getElementById("productNameInput");
 var productPriceInput = document.getElementById("productPriceInput");
 var productCategoryInput = document.getElementById("productCategoryInput");
 var productDescInput = document.getElementById("productDescInput");
+var searchInput = document.getElementById("searchInput");
 var tBody = document.getElementById("tBody");
 var currentIndex;
-var productsArr =[];
-if(JSON.parse(localStorage.getItem("productsArr")) != null){
-productsArr = JSON.parse(localStorage.getItem("productsArr"));
+var productsArr = [];
+if (JSON.parse(localStorage.getItem("productsArr")) != null) {
+  productsArr = JSON.parse(localStorage.getItem("productsArr"));
 }
 displayProducts();
 
@@ -18,26 +19,28 @@ function addProduct() {
     desc: productDescInput.value,
   };
 
-if (currentIndex == null) {
+  if (currentIndex == null) {
     productsArr.push(product);
-} else {
+  } else {
     productsArr[currentIndex] = product;
     currentIndex = null;
-    
+  }
+
+  localStorage.setItem("productsArr", JSON.stringify(productsArr));
+
+  displayProducts();
+  clearForm();
+  document.getElementById("addBtn").innerHTML = "Add Product";
 }
 
-localStorage.setItem("productsArr", JSON.stringify(productsArr))
+function displayProducts() {
+  var searchTerm = searchInput.value;
 
-displayProducts()
-clearForm();
-document.getElementById("addBtn").innerHTML = "Add Product";
-}
-
-function displayProducts(){
-      var trs = "";
+  var trs = "";
 
   for (var i = 0; i < productsArr.length; i++) {
-    trs += `
+    if (productsArr[i].name.toLowerCase().includes(searchTerm.toLowerCase())) {
+      trs += `
                     <tr>
                         <td>${i}</td>
                         <td>${productsArr[i].name}</td>
@@ -45,43 +48,37 @@ function displayProducts(){
                         <td>${productsArr[i].category}</td>
                         <td>${productsArr[i].desc}</td>
                         <td>
-                            <button onclick="updateProduct(${i})" class="btn btn-outline-warning">Update</button>
+                            <button onclick="patchProduct(${i})" class="btn btn-outline-warning">Update</button>
                         </td>
                         <td>
                             <button onclick="deleteProduct(${i})" class="btn btn-outline-danger">Delete</button>
                         </td>
                     </tr>
-`
+`;
+    }
   }
-
-  tBody.innerHTML = trs;
+    tBody.innerHTML = trs;
 }
 
-function deleteProduct(index){
-
-    productsArr.splice(index,1)
-localStorage.setItem("productsArr", JSON.stringify(productsArr))
-    displayProducts();
+function deleteProduct(index) {
+  productsArr.splice(index, 1);
+  localStorage.setItem("productsArr", JSON.stringify(productsArr));
+  displayProducts();
 }
 
-function updateProduct(index){
-    currentIndex=index;
-    productNameInput.value= productsArr[index].name;
-    productPriceInput.value=productsArr[index].price;
-    productCategoryInput.value=productsArr[index].category;
-    productDescInput.value=productsArr[index].desc;
-    document.getElementById("addBtn").innerHTML = "Update Product";
-
+function patchProduct(index) {
+  currentIndex = index;
+  productNameInput.value = productsArr[index].name;
+  productPriceInput.value = productsArr[index].price;
+  productCategoryInput.value = productsArr[index].category;
+  productDescInput.value = productsArr[index].desc;
+  document.getElementById("addBtn").innerHTML = "Update Product";
 }
 function clearForm() {
-    productNameInput.value = "";
-    productPriceInput.value = "";
-    productCategoryInput.value = "";
-    productDescInput.value = "";
+  productNameInput.value = "";
+  productPriceInput.value = "";
+  productCategoryInput.value = "";
+  productDescInput.value = "";
 }
-
-
-
-
 
 
